ceb27600366b233ed55cca48ad52d661a2fd5207
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-29 11:07:37 -0500

    Improve variable name for more clarity

--- balancedBrackets.js
+++ balancedBrackets.js
@@ -5,18 +5,18 @@ let isBalanced = (input) => {
   let stack = []
 
   for(let bracket of input) {
-    let bracketIndex = brackets.indexOf(bracket)
-    console.log(`input index: ${input.indexOf(bracket)}, the ${brackets[bracketIndex]} bracket which is the element at index ${bracketIndex} in the input string `)
+    let bracketsIndex = brackets.indexOf(bracket)
+    console.log(`input index: ${input.indexOf(bracket)}, the ${brackets[bracketsIndex]} bracket which is the element at index ${bracketsIndex} in the input string `)
 
-    if(bracketIndex === -1) {
+    if(bracketsIndex === -1) {
       continue
     }
 
-    if(bracketIndex % 2 === 0) {
-      stack.push(bracketIndex + 1)
-      console.log(`bracketIndex = ${bracketIndex}, the ${brackets[bracketIndex]} bracket was just added, so the stack is currently ${stack}`)
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+      console.log(`bracketsIndex = ${bracketsIndex}, the ${brackets[bracketsIndex]} bracket was just added, so the stack is currently ${stack}`)
     } else {
-      if(stack.length === 0 || stack.pop() !== bracketIndex) {
+      if(stack.length === 0 || stack.pop() !== bracketsIndex) {
         return false;
       }
     }
