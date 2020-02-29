f6c911857e3e916205cfc1460817164bbf76253f
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-29 13:38:38 -0500

    Remove unnecessary continue conditional

--- balancedBrackets.js
+++ balancedBrackets.js
@@ -6,15 +6,9 @@ let isBalanced = (input) => {
 
   for(let bracket of input) {
     let bracketsIndex = brackets.indexOf(bracket)
-    console.log(`input index: ${input.indexOf(bracket)}, the ${brackets[bracketsIndex]} bracket which is the element at index ${bracketsIndex} in the input string `)
-
-    if(bracketsIndex === -1) {
-      continue
-    }
 
     if(bracketsIndex % 2 === 0) {
       stack.push(bracketsIndex + 1)
-      console.log(`bracketsIndex = ${bracketsIndex}, the ${brackets[bracketsIndex]} bracket was just added, so the stack is currently ${stack}`)
     } else {
       if(stack.length === 0 || stack.pop() !== bracketsIndex) {
         return false;
