7b3f64000f075fca31a7f2248156c6bfd55b6297
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-29 10:58:32 -0500

    arrow function

--- balancedBrackets.js
+++ balancedBrackets.js
@@ -1,11 +1,12 @@
 
-function isBalanced(input){
+let isBalanced = (input) => {
 
   let brackets = "[]{}()<>"
   let stack = []
 
   for(let bracket of input) {
     let bracketIndex = brackets.indexOf(bracket)
+    console.log(`input index: ${input.indexOf(bracket)}, the ${brackets[bracketIndex]} bracket which is the element at index ${bracketIndex} in the input string `)
 
     if(bracketIndex === -1) {
       continue
@@ -13,6 +14,7 @@ function isBalanced(input){
 
     if(bracketIndex % 2 === 0) {
       stack.push(bracketIndex + 1)
+      console.log(`bracketIndex = ${bracketIndex}, the ${brackets[bracketIndex]} bracket was just added, so the stack is currently ${stack}`)
     } else {
       if(stack.length === 0 || stack.pop() !== bracketIndex) {
         return false;
