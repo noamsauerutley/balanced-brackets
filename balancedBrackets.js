20b5c45922b7cdc680965d3073c1b755246b2323
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-28 19:50:25 -0500

    tabbing

--- balancedBrackets.js
+++ balancedBrackets.js
@@ -1,26 +1,25 @@
 
 function isBalanced(input){
 
-let parentheses = "[]{}()<>"
-let stack = []
+  let brackets = "[]{}()<>"
+  let stack = []
 
-for(let parenthesis of input) {
-let parenthesisIndex = parentheses.indexOf(parenthesis);
+  for(let bracket of input) {
+    let bracketIndex = brackets.indexOf(bracket)
 
-if(parenthesisIndex === -1) {
-  continue;
-}
+    if(bracketIndex === -1) {
+      continue
+    }
 
-if(parenthesisIndex % 2 === 0) {
-  stack.push(parenthesisIndex + 1); // push next expected brace position
-} else {
-  if(stack.length === 0 || stack.pop() !== parenthesisIndex) {
-    return false;
+    if(bracketIndex % 2 === 0) {
+      stack.push(bracketIndex + 1)
+    } else {
+      if(stack.length === 0 || stack.pop() !== bracketIndex) {
+        return false;
+      }
+    }
   }
-}
-}
-
-return stack.length === 0;
+  return stack.length === 0
 }
 
 module.exports = isBalanced
